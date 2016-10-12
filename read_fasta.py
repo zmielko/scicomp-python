6cdeea8ed1b8fd7e6e274392e57e1c6ca263e845
Dan Leehr <dan.leehr@duke.edu>
2016-10-11 22:17:30 -0400

    Adds functions, scripts, and plotting

---
+++ read_fasta.py
@@ -0,0 +1,16 @@
+import sys
+
+def read_fasta(filename):
+    sequence = ''
+    f = open(filename)
+    for line in f:
+        line = line.strip()
+        if '>' in line:
+            pass
+        else:
+            # Append to the last sequence
+            sequence = sequence + line
+    f.close()
+    return sequence
+
+print read_fasta(sys.argv[1])
